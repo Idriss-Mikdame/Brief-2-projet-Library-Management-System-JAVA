58ea645fa5d6b0d65c90a4f9b486930922cec48a
Idriss-Mikdame <mikdameidriss420@gmail.com>
2024-12-16 15:42:01 +0100

    creation de class Main principal et class BOOK

---
+++ src/Book.java
@@ -0,0 +1,32 @@
+public class Book {
+     String titre;
+     String auteur;
+     String isbn;
+     boolean disponible;
+
+    public Book(String titre, String auteur, String isbn, boolean disponible) {
+        this.titre = titre;
+        this.auteur = auteur;
+        this.isbn = isbn;
+        this.disponible = disponible;
+    }
+
+    public String getTitre() {return titre;}
+    public void setTitre(String titre) {this.titre = titre;}
+    public String getAuteur() {return auteur;}
+    public void setAuteur(String auteur) {this.auteur = auteur;}
+    public String getIsbn() {return isbn;}
+    public void setIsbn(String isbn) {this.isbn = isbn;}
+    public boolean isDisponible() {return disponible;}
+    public void setDisponible(boolean disponible) {this.disponible = disponible;}
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "titre='" + titre + '\'' +
+                ", auteur='" + auteur + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", disponible=" + disponible +
+                '}';
+    }
+}
